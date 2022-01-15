151d290f3838503bb1759d60e90d8ff28bf804bf
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-15 15:31:42 +0530

    main section content added

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,5 +1,24 @@
+/**
+Challenge: Build the main section!
+
+Skip 2 aspects of the design for now:
+1. The colored bullets in the list
+2. The larger React logo on the side
+
+Those will be separate challenges coming up.
+*/
+
 export default function Navbar(){
     return (
-        <h1>Main content goes here</h1>
+        <div className="main">
+            <h1>Fun facts about react</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
     )
 }
