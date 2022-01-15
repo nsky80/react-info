ff05f5ab525cba38ebdd664a625dfa0c90816746
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-15 13:13:10 +0530

    Project initialized

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+Challenge: Project setup
 
-function Header(){
-  return (
-          <header>
-              <nav className="nav">
-                  <img src={logo} className="nav-logo" /><h1 className='logo-text'>ReactFacts</h1>
-                  <div className="nav-items">
-                    <h3>React Course - Project 1</h3>
-                  </div>
-              </nav>
-          </header>
-  )
-}
+- Create a `components` folder
+- Create the following components in separate files inside
+  the components folder.  In each one, just render an `h1` 
+  with the name of the component (e.g. return <h1>Navbar goes here</h1>):
+    - Navbar
+    - Main
+- Create an App component outside the components folder (sibling to
+  the index.js file)
+    - Have App render the Navbar and Main components
+- Import and render the App component inside of index.js using ReactDOM
+    - At this point you should have your "Navbar goes here" etc. showing up
+      in the mini-browser.
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the @import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this 
+  works if you haven't done it before)
+*/
+import Navbar from "./component/Navbar";
+import Main from "./component/Main";
 
-function Footer(){
-  return (
-  <footer className="footer">
-      <small>© 2022 NSKY80 development. All rights reserved.</small>
-  </footer>
-  )
-}
 
-function MainContent(){
-  return (
-      <div className='body-items'>
-          <h1>Fun facts about React</h1>
-          <ul>
-              <li>Was first released in 2013</li>
-              <li>Was originally created by Jordan Walke</li>
-              <li>Has well over 100K stars on GitHub</li>
-              <li>Is maintained by Facebook</li>
-              <li>Powers thousands of enterprise apps, including mobile apps</li>
-          </ul>
-      </div>
-  )
-}
-
-export default function Page() {
+export default function App() {
   return (
       <div>
-          <Header />
-          <MainContent />
-          <Footer />
+          <Navbar />
+          <Main />
       </div>
   )
 }
