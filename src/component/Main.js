73ec746e499577cef6ad0358f99f50ca8b988dbc
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-15 15:51:37 +0530

    added style to main content

--- src/component/Main.js
+++ src/component/Main.js
@@ -10,15 +10,15 @@ Those will be separate challenges coming up.
 
 export default function Navbar(){
     return (
-        <div className="main">
-            <h1>Fun facts about react</h1>
-            <ul>
+        <main className="main">
+            <h1 className="main--title">Fun facts about react</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
